b9518e33f463f3222b92a99d2edbae95b287e84b
Ritesh Toppo <ritesh.toppo@gmail.com>
2020-07-28 11:32:18 +0530

    Add files via upload
    Code for javascript selenium

---
+++ config.js
@@ -0,0 +1,14 @@
+exports.config = {
+    seleniumAddress: 'https://localhost:4445/wd/hub',
+  
+  // add more browsers to this array for more parallel tests!!
+  multiCapabilities: [{
+    'browserName': 'firefox',    
+    'platform': 'WIN10',       
+    'version':'62.0',
+  
+  }],
+  
+  //Add test scripts in this array to execute in parallel
+  specs: ['spec.js']
+};
